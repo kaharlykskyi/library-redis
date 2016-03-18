a9ecfb4eafe548a1e32fa9b82ce0d483061e6147
ipanardian <ipan.software@gmail.com>
2016-03-18 11:28:17 +0700

    CRUD Customer

--- config/web.php
+++ config/web.php
@@ -8,8 +8,7 @@ $config = [
     'bootstrap' => ['log'],
     'components' => [
         'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => '686%%^%^(*HIUBKY*&^%&^%&*(*NKH@808295576309%$%^$^$&*bbbkkk',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
